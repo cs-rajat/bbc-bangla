f78fa3c3e43319e86ae8761d16d4557e9a13bdd3
Rahul <cs.rajat75@gmail.com>
2025-09-06 23:47:41 +0600

    bookmark Section added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const categoryContainer = document.getElementById('categoryContainer')
 const newsContainer = document.getElementById('newsContainer')
+const bookmarkContainer = document.getElementById('bookmarkContainer')
+let bookmark =[];
 
 const loadCategory = () =>{
 
@@ -36,7 +38,7 @@ const showCategory = (categories) =>{
     })
     
     const loadNewsByCategory = (categoryId) => {
-       console.log(categoryId)
+    //    console.log(categoryId)
      fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
          .then(res => res.json())
          .then(data => showNews(data.articles))
@@ -45,21 +47,59 @@ const showCategory = (categories) =>{
 
     }
     const showNews = (articles) =>{
-        console.log(articles)
+        // console.log(articles)
     newsContainer.innerHTML="" 
     articles.forEach(article =>{
         newsContainer.innerHTML+= `
-        <div>
+        <div class="border border-gray-300 rounded-b-md">
             <div>
-            <img src="${article.image.srcset[5].url}">
+            <img class="w-full block" src="${article.image.srcset[5].url}">
             </div>
-           <h1>${article.title}</h1>
-           <p>${article.time}</p>
+            <div id="${article.id}" class="p-2">
+                 <h1 class="font-bold">${article.title}</h1>
+                 <p class="text-gray-500 my-2">${article.time}</p>
+                 <button class="btn">Bookmark</button>
+            </div>
+         
         </div>
 
         `
       })
 }
+newsContainer.addEventListener('click',  (e)=>{
+    //   console.log(e.target);
+    if(e.target.innerText==='Bookmark'){
+        // console.log('Bookmark Button Clicked')
+       handleBookmark(e)
+    }
+        
+})
+
+const handleBookmark = (e)=> {
+    const title = e.target.parentNode.children[0].innerText;
+        const id = e.target.parentNode;
+        console.log(id);
+
+        bookmark.push({
+            title : title,
+            id : id,
+        })
+        showBookmark(bookmark)
+}
+
+const showBookmark= (bookmark) =>{
+    console.log(bookmark)
+    bookmarkContainer.innerHTML='';
+    bookmark.forEach(book => {
+        bookmarkContainer.innerHTML+=`
+        <div class="border border-gray-300 p-2">
+            <h1>${book.title}</h1>
+        </div>
+        `
+    })
+
+}
+
 
 
 loadCategory();
