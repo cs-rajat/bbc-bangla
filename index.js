939cb65ae97b5651314138ff773a916d9cd85da8
Rahul <cs.rajat75@gmail.com>
2025-09-06 21:23:16 +0600

    News catogory and news article updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const categoryContainer = document.getElementById('categoryContainer')
+const newsContainer = document.getElementById('newsContainer')
 
 const loadCategory = () =>{
 
@@ -13,23 +14,53 @@ const loadCategory = () =>{
 const showCategory = (categories) =>{
     // console.log(categories)
     categories.forEach(cat => {
-        categoryContainer.innerHTML += `<li class="border-red-600">${cat.title}</li>`
+        categoryContainer.innerHTML += `<li id="${cat.id}" class="border-red-600">${cat.title}</li>`
     });
+ }
+
     categoryContainer.addEventListener('click', (e) =>{
 
         const allLi = document.querySelectorAll('li')
-        console.log(allLi)
+        // console.log(allLi)
 
         allLi.forEach(li=>{
             li.classList.remove('border-b-4')
         })
         
         if(e.target.localName === 'li'){
-          console.log(e.target);
+        //   console.log(e.target.id);
           e.target.classList.add('border-b-4')
+          loadNewsByCategory(e.target.id)
         }
 
-    })    
+    })
+    
+    const loadNewsByCategory = (categoryId) => {
+       console.log(categoryId)
+     fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+         .then(res => res.json())
+         .then(data => showNews(data.articles))
+
+      .catch(err => console.log(err))
+
+    }
+    const showNews = (articles) =>{
+        console.log(articles)
+    newsContainer.innerHTML="" 
+    articles.forEach(article =>{
+        newsContainer.innerHTML+= `
+        <div>
+            <div>
+            <img src="${article.image.srcset[5].url}">
+            </div>
+           <h1>${article.title}</h1>
+           <p>${article.time}</p>
+        </div>
+
+        `
+      })
 }
 
+
 loadCategory();
+loadNewsByCategory('main');
